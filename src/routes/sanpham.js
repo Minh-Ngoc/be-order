37dfc16f158b7d9b5901161d8e6d8660d1be784c
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-19 13:46:31 +0700

    upda

--- src/routes/sanpham.js
+++ src/routes/sanpham.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 
 const SanPhamController = require('../app/controllers/SanPhamController');
 
-router.post('/create', SanPhamController.create);
-router.get('/list', SanPhamController.list);
-router.get('/:id/edit', SanPhamController.edit);
-router.put('/:id', SanPhamController.update);
-router.delete('/:id', SanPhamController.destroy);
+router.post('/create', cors(), SanPhamController.create);
+router.get('/list', cors(), SanPhamController.list);
+router.get('/:id/edit', cors(), SanPhamController.edit);
+router.put('/:id', cors(), SanPhamController.update);
+router.delete('/:id', cors(), SanPhamController.destroy);
 
 module.exports = router;
