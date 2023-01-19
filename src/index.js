eeafeda442022b27f01a1da0459ef152cae43d6a
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-19 13:38:14 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -26,16 +26,16 @@ db.connect();
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
-    );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
+    // res.setHeader(
+    //   "Access-Control-Allow-Headers",
+    //   "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    // );
+    // res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.setHeader(
+    //   "Access-Control-Allow-Methods",
+    //   "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    // );
     next();
   });
 
