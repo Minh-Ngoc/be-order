2be63b3ae8d3ff6180cc53e5f4f6e42ea2d90728
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-20 14:33:45 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config({path: __dirname + '/.env' })
-const serverless = require("serverless-http");
 
 const path = require('path');
 const express = require('express');
@@ -19,7 +18,7 @@ app.use(
     cors()
   );
 
-app.use(express.json());
+app.use(express());
 // Connect to DB
 
 db.connect();
