0cf8a5f68799f5c95dd4d1df817c2c4076a043ec
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-20 22:04:20 +0700

    update

--- src/index.js
+++ src/index.js
@@ -88,3 +88,37 @@ app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`),
 );
 
+const welcomeMessage = (req, res) => {
+    res.send(` 
+    <head>
+        <title>Server On! ✅</title> 
+        <link rel="icon" href="/fav.ico">
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+            body {
+                text-align       : center;
+                background-color : #eee;
+            }
+            h1{
+                width         : max-content;
+                margin        : 40px auto 00px; 
+                padding       : 20px 30px 24px;
+                font-family   : 'Nunito', sans-serif;
+                font-size     : 42px;
+                color         : #111;
+                border        : 1px solid black;
+                border-radius : 3px;
+            }
+            img{
+                width  : 750px;
+                height : 750px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1> Memories App - Server is running... ✅ </h1>
+        <img src='server.png'/>
+    </body>
+    `);
+}
+app.get('/', welcomeMessage);
