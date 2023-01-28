2cec917df157f9e92d0aea1a10f1e5972679d0e1
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-28 12:16:45 +0700

    first commit

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -7,8 +7,6 @@ const mongodb = require('mongodb');
 
 const URI = 'mongodb+srv://ngocminh:Minh1805701@cluster0.31wphgu.mongodb.net/?retryWrites=true&w=majority';
 // const URI = 'mongodb://localhost:27017/BillManage';
-const client = await mongodb.MongoClient.connect(URI);
-const conn = mongoose.createConnection().setClient(client);
 
 const connect = async (e) => {
     try {
@@ -17,8 +15,7 @@ const connect = async (e) => {
         //     useUnifiedTopology: true,
         //     serverApi: ServerApiVersion.v1,
         // });
-        conn.readyState(1); // 1, means 'CONNECTED'
-
+        mongodb.MongoClient.connect(URI);
         console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect failure!!!');
