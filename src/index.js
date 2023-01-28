53af7d9042582da455509397156fa095a97f9838
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-28 11:02:38 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -15,19 +15,19 @@ app.use(
     cors()
   );
 
-app.use(express);
+// app.use(express);
 // Connect to DB
 
 // db.connect();
 
 // Curb Cores Error by adding a header here
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-});
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//     next();
+// });
 
 // const port = process.env.PORT || 3001;
 
