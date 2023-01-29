f5903ef1f9418025f4fd0bbc5bb426ef3e8f768c
ngocminh101100 <ngocminh101100@gmail.com>
2023-01-29 08:14:00 +0700

    first commit

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,24 +1,42 @@
-// require('dotenv').config();
-
 // const mongoose = require('mongoose');
 // const ServerApiVersion  = require('mongoose');
-const { MongoClient } = require("mongodb");
 
+// const URI = 'mongodb+srv://ngocminh:Minh1805701@cluster0.31wphgu.mongodb.net/?retryWrites=true&w=majority';
+
+// const connect = async (e) => {
+//     try {
+//         // const URI = 'mongodb://localhost:27017/BillManage';
+//         mongoose.connect(await URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             serverApi: ServerApiVersion.v1,
+//         });
+        
+//         console.log('Connect successfully!!!');
+//     } catch (error) {
+//         console.log('Connect failure!!!');
+//     }
+// }
+// module.exports = { connect };
+
+const mongodb = require("mongodb");
 const URI = 'mongodb+srv://ngocminh:Minh1805701@cluster0.31wphgu.mongodb.net/?retryWrites=true&w=majority';
 
-const connect = async (e) => {
-    try {
-        // const URI = 'mongodb://localhost:27017/BillManage';
-        // return mongoose.connect(await URI, {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        //     serverApi: ServerApiVersion.v1,
-        // });
-        return new MongoClient(URI).connect();
-        console.log('Connect successfully!!!');
-    } catch (error) {
-        console.log('Connect failure!!!');
-    }
-}
+exports.handler = async function (event, context) {
+  const client = await mongodb.connect(URI, { useUnifiedTopology: true })
 
-module.exports = { connect } ;
+  try {
+    client.close()
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: "Please try again later."
+    }
+  }
+}
